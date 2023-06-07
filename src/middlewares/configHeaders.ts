c4fee8ba010d6ae54209161f6f1d9fd4dac70729
CaioAlves <caio.alves@casadoconstrutor.com.br>
2023-06-06 22:44:52 -0300

    teste headers 1

--- src/middlewares/configHeaders.ts
+++ src/middlewares/configHeaders.ts
@@ -1,8 +1,17 @@
 import { NextFunction, Request, Response } from "express"
 
 export const configHeaders = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.hostname)
+  const whitelist: Array<string | undefined> = [
+    "https://app-mhd.pages.dev",
+    "https://app-mhd.vercel.app"
+  ]
   if (process.env.AMBIENT === "production") {
-    res.setHeader("Access-Control-Allow-Origin", "https://app-mhd.pages.dev")
+
+    if (whitelist.indexOf(req.hostname) !== -1) {
+      res.setHeader("Access-Control-Allow-Origin", req.hostname)
+    }
+    // res.setHeader("Access-Control-Allow-Origin", "https://app-mhd.pages.dev")
   }
 
   next()
